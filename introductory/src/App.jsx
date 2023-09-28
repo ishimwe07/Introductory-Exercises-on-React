59613fa065c668a2e0f1b75ca80c7c5d3627b318
Ishimwe Gabby <ishimwgabby@gmail.com>
2023-09-28 13:32:11 +0200

    Completed the exercise 4 about counting the number of times abutton was clicked.

--- introductory/src/App.jsx
+++ introductory/src/App.jsx
@@ -1,9 +1,21 @@
+import React from "react";
+import Button from "./buttonComponent"
+import HeroText from "./heroText"
 
+export default function App() {
 
-function App() {
+  const [counts, setCounts] = React.useState(0);
+
+  function handleClick(){
+    setCounts(prevCount => prevCount + 1);
+  }
+
     return (
-    <h1 className="bg-blue-300">Hello there!</h1>
+      <div className="mx-[30%] my-[10%] grid justify-items-center gap-5">
+        <HeroText counts = {counts} />
+        <Button handleClick={handleClick} />
+      </div>
   )
 }
 
-export default App
+
