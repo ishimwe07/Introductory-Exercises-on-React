eb5bc5043a32999de1f4b275354d4d2065072d31
Ishimwe Gabby <ishimwgabby@gmail.com>
2023-09-29 09:25:40 +0200

    Completed the exercise 9.

--- introductory/src/App.jsx
+++ introductory/src/App.jsx
@@ -5,12 +5,13 @@ function App() {
 
   const [data, setData] = React.useState([]);
   
-
-  React.useEffect(()=>{
+  function fetchData(){
     fetch("https://random-data-api.com/api/users/random_user?size=10")
         .then(response=> response.json())
         .then(data=> setData(data));
-  },[])
+  }
+  
+  React.useEffect(fetchData,[])
 
 
    const components = data.map(el=> <Card key={el.uid} 
@@ -18,15 +19,20 @@ function App() {
                                           description={`Picture of ${el.address.street_name}`}
                                           job={el.employment.title}
                                           name={el.address.street_name}
+                                          firstName={el.first_name}
+                                          lastName={el.last_name}
+                                          keySkills={el.employment.key_skill}
+                                          phoneNumber={el.phone_number}
+                                          gender={el.gender}
                                           />)
   
   return (
     <div>
         <button
         className="px-12 py-3 mt-10 mx-[40%] bg-red-600 hover:bg-red-500 active:bg-red-700 border-b-4 border-black rounded-xl"
-
+        onClick={fetchData}
         >Fetch Random</button>
-        <div className="grid sm:grid-cols-2 justify-items-center gap-10 mt-5 mx-[5%] bg-slate-300 rounded-md py-2">
+        <div className="grid sm:grid-cols-2 justify-items-center gap-10 mt-5 mx-[5%] bg-slate-300 rounded-md py-10">
           {components}
         </div>
     </div>
--- introductory/src/card.jsx
+++ introductory/src/card.jsx
@@ -1,11 +1,35 @@
 
 export default function Card(props){
-    console.log(props)
     return (
-    <div className="bg-blue-100 rounded-2xl p-10 shadow-xl shadow-blue-500">
-        <img className="rounded-2xl" src={props.image} alt={props.description} />
-        <h1 className="text-2xl font-bold">{props.name}</h1>
-        <p className="text-xl font-medium">{props.job}</p>
-    </div>
+        <div className="flip-card">
+            <div className="flip-card-inner">
+                <div className="flip-card-front">
+                    <div className="bg-blue-100 rounded-2xl p-10 shadow-xl hover:shadow-blue-800 shadow-blue-500">
+                        <img className="rounded-2xl" src={props.image} alt={props.description} />
+                        <h1 className="text-2xl font-bold">{props.name}</h1>
+                        <p className="text-xl font-medium">{props.job}</p>
+                    </div>
+                </div>
+
+                <div className="flip-card-back bg-blue-400 rounded-xl p-5 text-xl text-slate-900">
+                    <h1>First Name: {props.firstName}</h1>
+                    <h1>Last Name: {props.lastName}</h1>
+                    <h1>Gender: {props.gender}</h1>
+                    <h1>Employment title: {props.job} </h1>
+                    <h1>Key Skills: {props.keySkills}</h1>
+                    <h1>Phone Number: <br /> {props.phoneNumber}</h1>
+                </div>
+            </div>
+        </div>
     )
 }
+
+
+
+    // <div className="bg-blue-100 rounded-2xl p-10 shadow-xl hover:shadow-blue-800 shadow-blue-500">
+    //     <img className="rounded-2xl" src={props.image} alt={props.description} />
+    //     <h1 className="text-2xl font-bold">{props.name}</h1>
+    //     <p className="text-xl font-medium">{props.job}</p>
+    // </div>
+
+    
