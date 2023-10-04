9f4be21b81ec167aac4003df2792c467cd2ee5e3
Ishimwe Gabby <ishimwgabby@gmail.com>
2023-10-04 18:56:01 +0200

    Updates: fixed the recommended changes about being flexible on the choice of function types I use, if it is arrow I stick to that or if I choose regular I stick to that too.

--- introductory/src/App.jsx
+++ introductory/src/App.jsx
@@ -1,9 +1,8 @@
 import React from "react"
-
 function App() {
     const [names, setNames] = React.useState({firstName:"",lastName:""})
 
-    function handleChange(event){
+    const handleChange = (event) =>{
       const {name, value} = event.target;
       setNames(prevState => ({...prevState, [name]:value}))
     }
@@ -24,19 +23,22 @@ function App() {
             onChange={(e)=>handleChange(e)}
             />
 
-    <input 
-        className="border-2 border-blue-500 active:border-black py-2 pr-10 rounded-md"
-        type="text" 
-        placeholder="Last Name" 
-        value={names.lastName} 
-        name="lastName" 
-        onChange={(e)=>handleChange(e)}/>
+      <input 
+          className="border-2 border-blue-500 active:border-black py-2 pr-10 rounded-md"
+          type="text" 
+          placeholder="Last Name" 
+          value={names.lastName} 
+          name="lastName" 
+          onChange={(e)=>handleChange(e)}
+        />
 
 
-    <button
-    className="text-3xl text-white bg-red-600 hover:bg-red-500 active:bg-red-800 py-2 px-12 rounded-xl border-b-4 border-black"
-    onClick={greetMe}
-    >GREET ME</button>
+      <button
+      className="text-3xl text-white bg-red-600 hover:bg-red-500 active:bg-red-800 py-2 px-12 rounded-xl border-b-4 border-black"
+      onClick={greetMe}
+      >
+        GREET ME
+      </button>
   </div>
   )
 }
