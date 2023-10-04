1283ef12b1b3117f40126123371b698224a7a9f6
Ishimwe Gabby <ishimwgabby@gmail.com>
2023-10-04 18:52:00 +0200

    Updates: fixed the recommended changes about removing empty unnecessary spaces.

--- introductory/src/listComponent.jsx
+++ introductory/src/listComponent.jsx
@@ -2,7 +2,7 @@ export default function List(props){
     return(
         <div className="flex gap-3 border-2 w-48 border-slate-950 p-4">
             <img src={`/${props.name}.jpg`} alt={`${props.name} Image`} className="h-10" />
-            <h1 className='text-2xl'>{props.name}</h1>
+            <h1 className=  "text-2xl  ">{props.name}</h1>
         </div>
     )
 }
