485f199028a4dc69e00c0befc3ae34a16a91c10e
Ishimwe Gabby <ishimwgabby@gmail.com>
2023-10-04 18:33:33 +0200

    Updates to remove the unnecessary spaces in my codes.

--- introductory/src/App.jsx
+++ introductory/src/App.jsx
@@ -5,13 +5,15 @@ function App() {
     alert("Clicked!");
   }
     return (
-    <button 
-      className=
-      "px-16 py-5 bg-slate-600 hover:bg-slate-400 active:bg-slate-800 text-amber-100 rounded-lg mx-[40%] my-[20%]"
-      onClick={handleClick}
-      >
-        Click Me
-      </button>
+      <div className="bg-red-100 h-screen flex justify-center items-center ">
+        <button 
+          className=
+          "px-16 py-5 bg-slate-600 hover:bg-slate-400 active:bg-slate-800 text-amber-100 rounded-lg"
+          onClick={handleClick}
+          >
+            Click Me
+          </button>
+      </div>
   )
 }
 
