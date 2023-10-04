9261f42ce3db02e7df461f52808588fc98329a44
Ishimwe Gabby <ishimwgabby@gmail.com>
2023-10-04 18:45:53 +0200

    Updates: fixed the recommended changes about removing empty unnecessary spaces.

--- introductory/src/App.jsx
+++ introductory/src/App.jsx
@@ -1,9 +1,8 @@
 import List from "./listComponent";
-
 function App() {
-  const arrayOfItems = ['dog', 'cat', 'chicken', 'cow', 'sheep', 'horse'];
+  
+  const arrayOfItems = [  "dog  ",   "cat  ",   "chicken  ",   "cow  ",   "sheep  ",   "horse  "];
   const arrayOfComponents = arrayOfItems.map((element, index) => <List key={index} element={element} />)
-
 
     return (
     <ul className="text-2xl marker:bg-yellow-600 mx-40 my-10 p-10 bg-blue-300 rounded-lg list-disc">
