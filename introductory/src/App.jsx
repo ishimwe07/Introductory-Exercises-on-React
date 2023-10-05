b085d0eedecf0f2520b7ae03e16a43b3a938707f
Ishimwe Gabby <ishimwgabby@gmail.com>
2023-10-05 17:51:06 +0200

    removed unnecessary spaces from the animal list.

--- introductory/src/App.jsx
+++ introductory/src/App.jsx
@@ -3,7 +3,7 @@ import List from "./listComponent"
 
 function App() {
 
-  const arrayOfAnimals = [  "dog  ",   "cat  ",   "chicken  ",   "cow  ",   "sheep  ",   "horse  "];
+  const arrayOfAnimals = [  "dog",   "cat",   "chicken",   "cow",   "sheep",   "horse"];
   const arrayOfComponents = arrayOfAnimals.map((el, index)=> <List key={index} name={el} />)
     return (
     <div className="m-5">
