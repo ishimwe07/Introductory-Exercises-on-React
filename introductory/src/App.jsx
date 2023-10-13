767714f8ce615cf76dc930dc71ae73fa2cd78cb9
Ishimwe Gabby <ishimwgabby@gmail.com>
2023-10-13 16:33:20 +0200

    Completed working on the reviews from peer

--- introductory/src/App.jsx
+++ introductory/src/App.jsx
@@ -1,15 +1,13 @@
 import data from "./data.json";
 import JokesComponent from "./jokeComponent";
 
-
 function App() {
-
-    const jokes = data.map(el=> <JokesComponent key={el.id} setup={el.setup} punchline={el.punchline} />)
-    return (
-    <div className="mx-[10%] my-[2%] grid grid-cols-2 gap-10">
-        {jokes}
-    </div>
-  )
+  const jokes = data.map((el) => (
+    <JokesComponent key={el.id} setup={el.setup} punchline={el.punchline} />
+  ));
+  return (
+    <div className="mx-[10%] my-[2%] grid grid-cols-2 gap-10">{jokes}</div>
+  );
 }
 
-export default App
+export default App;
--- introductory/src/jokeComponent.jsx
+++ introductory/src/jokeComponent.jsx
@@ -1,8 +1,8 @@
-export default function JokesComponent(props){
-    return(
-        <div className=" text-center bg-slate-100 rounded-lg shadow-2xl shadow-slate-400 border-red-200 border-2 py-8 px-5" >
-            <h1 className="text-3xl font-bold">{props.setup}</h1>
-            <p className="text-xl font-normal mt-5">{props.punchline}</p>
-        </div>
-    )
+export default function JokesComponent(props) {
+  return (
+    <div className=" text-center bg-slate-100 rounded-lg shadow-2xl shadow-slate-400 border-red-200 border-2 py-8 px-5">
+      <h1 className="text-3xl font-bold">{props.setup}</h1>
+      <p className="text-xl font-normal mt-5">{props.punchline}</p>
+    </div>
+  );
 }
