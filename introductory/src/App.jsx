87a8f2d6fccb865f8d8a51ad678aac87f13cc0ae
Ishimwe Gabby <ishimwgabby@gmail.com>
2023-10-16 09:01:48 +0200

    fix:Worked on the reviews I received about formatting my codes for clear readability.

--- introductory/src/App.jsx
+++ introductory/src/App.jsx
@@ -1,11 +1,10 @@
+import { nanoid } from "nanoid";
 import List from "./listComponent";
 
-function App() {
+export default function App() {
   const arrayOfAnimals = ["dog", "cat", "chicken", "cow", "sheep", "horse"];
-  const arrayOfComponents = arrayOfAnimals.map((el, index) => (
-    <List key={index} name={el} />
+  const arrayOfComponents = arrayOfAnimals.map((animal) => (
+    <List key={nanoid()} name={animal} />
   ));
   return <div className="m-5">{arrayOfComponents}</div>;
 }
-
-export default App;
--- introductory/src/listComponent.jsx
+++ introductory/src/listComponent.jsx
@@ -1,8 +1,12 @@
-export default function List(props){
-    return(
-        <div className="flex gap-3 border-2 w-48 border-slate-950 p-4">
-            <img src={`/${props.name}.jpg`} alt={`${props.name} Image`} className="h-10" />
-            <h1 className=  "text-2xl  ">{props.name}</h1>
-        </div>
-    )
+export default function List(props) {
+  return (
+    <div className="flex gap-3 border-2 w-48 border-slate-950 p-4">
+      <img
+        src={`/${props.name}.jpg`}
+        alt={`${props.name} Image`}
+        className="h-10"
+      />
+      <h1 className="text-2xl">{props.name}</h1>
+    </div>
+  );
 }
