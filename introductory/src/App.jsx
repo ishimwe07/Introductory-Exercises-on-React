8b67c499f592cb250a4ce1d7ac7fb9171c10f751
Ishimwe Gabby <ishimwgabby@gmail.com>
2023-10-16 08:49:39 +0200

    fix:Worked on the reviews I received about formatting my codes and consistency in exporting.

--- introductory/src/App.jsx
+++ introductory/src/App.jsx
@@ -1,14 +1,21 @@
+import { nanoid } from "nanoid";
 import List from "./listComponent";
-function App() {
-  
-  const arrayOfItems = [  "dog  ",   "cat  ",   "chicken  ",   "cow  ",   "sheep  ",   "horse  "];
-  const arrayOfComponents = arrayOfItems.map((element, index) => <List key={index} element={element} />)
+export default function App() {
+  const arrayOfItems = [
+    "dog  ",
+    "cat  ",
+    "chicken  ",
+    "cow  ",
+    "sheep  ",
+    "horse  ",
+  ];
+  const arrayOfComponents = arrayOfItems.map((element) => (
+    <List key={nanoid()} element={element} />
+  ));
 
-    return (
+  return (
     <ul className="text-2xl marker:bg-yellow-600 mx-40 my-10 p-10 bg-blue-300 rounded-lg list-disc">
       {arrayOfComponents}
     </ul>
-  )
+  );
 }
-
-export default App
